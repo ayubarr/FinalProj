15cbb25360220e3630db74afc1e5e0abc09a24f5
ayubarr <114123666+ayubarr@users.noreply.github.com>
2023-05-28 01:56:55 +0600

    IClient Service edited

--- FinalProj.Services/Interfaces/IClientService.cs
+++ FinalProj.Services/Interfaces/IClientService.cs
@@ -1,17 +1,14 @@
-﻿using FinalApp.ApiModels.DTOs.EntitiesDTOs.RequestsDTO;
-using FinalApp.ApiModels.DTOs.EntitiesDTOs.UsersDTOs;
-using FinalApp.ApiModels.Response.Interfaces;
+﻿using FinalApp.ApiModels.Response.Interfaces;
 using FinalApp.Domain.Models.Entities.Persons.Users;
-using FinalApp.Domain.Models.Entities.Requests.RequestsInfo;
 
 namespace FinalApp.Services.Interfaces
 {
     public interface IClientService
     {
-        public Task<IBaseResponse<bool>> CreateClient(Client client);
+        //public Task<IBaseResponse<bool>> CreateClient(Client client);
         public Task<IBaseResponse<IEnumerable<Client>>> GetClientsWithRequests();
         //public Task<IBaseResponse<IEnumerable<Request>>> GetActiveRequests(string clientId);
         //public Task<IBaseResponse<IEnumerable<Request>>> GetClosedRequests(string clientId);
-         
+
     }
 }
