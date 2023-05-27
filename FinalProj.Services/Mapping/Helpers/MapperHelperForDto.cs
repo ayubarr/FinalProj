0764abc4404b33ff09355fe415a28be212f5307e
ayubarr <114123666+ayubarr@users.noreply.github.com>
2023-05-27 23:40:00 +0600

    ADD: Auto mapper added

---
+++ FinalProj.Services/Mapping/Helpers/MapperHelperForDto.cs
@@ -0,0 +1,41 @@
+﻿using FinalApp.ApiModels.DTOs.CommonDTOs.BaseDTOs;
+using FinalApp.Domain.Models.Abstractions.BaseEntities;
+using FinalApp.Services.Mapping.Config;
+using FinallApp.ValidationHelper;
+
+namespace FinalApp.Services.Mapping.Helpers
+{
+    /// <summary>
+    /// Helper class for mapping between source entities of type T and destination DTOs of type Tmodel.
+    /// </summary>
+    /// <typeparam name="T">The type of the source entity.</typeparam>
+    /// <typeparam name="Tmodel">The type of the destination DTO.</typeparam>
+    public static class MapperHelperForDto<T, Tmodel>
+        where T : BaseEntity
+        where Tmodel : BaseEntityDTO
+    {
+        /// <summary>
+        /// Maps a collection of source entities to a collection of destination DTOs.
+        /// </summary>
+        /// <param name="sourceCollection">The collection of source entities.</param>
+        /// <returns>A collection of destination DTOs.</returns>
+        public static IEnumerable<Tmodel> Map(IEnumerable<T> sourceCollection)
+        {
+
+            ObjectValidator<IEnumerable<T>>.CheckIsNotNullObject(sourceCollection);
+            var mapper = AutoMapperConfig<T, Tmodel>.Initialize();
+            return mapper.Map<IEnumerable<Tmodel>>(sourceCollection);
+        }
+        /// <summary>
+        /// Maps a single source entity to a destination DTO.
+        /// </summary>
+        /// <param name="source">The source entity.</param>
+        /// <returns>The destination DTO.</returns>
+        public static Tmodel Map(T source)
+        {
+            ObjectValidator<T>.CheckIsNotNullObject(source);
+            var mapper = AutoMapperConfig<T, Tmodel>.Initialize();
+            return mapper.Map<Tmodel>(source);
+        }
+    }
+}
---
+++ FinalProj.Services/Mapping/UsersMap/MappingProfile.cs
@@ -0,0 +1,19 @@
+﻿using AutoMapper;
+using FinalApp.ApiModels.DTOs.CommonDTOs.BaseDTOs;
+using FinalApp.ApiModels.DTOs.EntitiesDTOs.UsersDTOs;
+using FinalApp.Domain.Models.Abstractions.BaseUsers;
+using FinalApp.Domain.Models.Entities.Persons.Users;
+
+namespace FinalApp.Services.Mapping.UsersMap
+{
+    public class MappingProfile : Profile
+    {
+        public MappingProfile()
+        {
+            CreateMap<User, BaseUserDTO>();
+            CreateMap<Client, ClientDTO>();
+            CreateMap<TechTeam, TechTeamDTO>();
+            CreateMap<SupportOperator, SupportOperatorDTO>();
+        }
+    }
+}
