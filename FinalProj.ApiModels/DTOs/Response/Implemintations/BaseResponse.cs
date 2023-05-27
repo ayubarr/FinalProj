bf657d6eccea05d61ae9435ba63ba5edad313b47
ayubarr <114123666+ayubarr@users.noreply.github.com>
2023-05-27 23:02:19 +0600

    ADD: Response added

---
+++ FinalProj.ApiModels/DTOs/Response/Implemintations/BaseResponse.cs
@@ -0,0 +1,87 @@
+﻿using FinalApp.ApiModels.Response.Interfaces;
+using FinallApp.ValidationHelper;
+using Newtonsoft.Json;
+
+namespace FinalApp.ApiModels.Response.Implemintations
+{
+    public class BaseResponse<T> : IBaseResponse<T>
+    {
+        private T _data;
+
+        public T Data
+        {
+            get
+            {
+                return _data;
+            }
+  
+            set
+            {
+                try
+                {
+                    ObjectValidator<T>.CheckIsNotNullObject(value);
+                    _data = value;
+                }
+                catch(ArgumentNullException exception)
+                {
+                    _message += $"\nERROR: {exception}";
+                }
+            }
+        }
+
+
+        private int _statusCode;
+
+        public int StatusCode
+        {
+            get
+            {
+                return _statusCode;
+            }
+            set
+            {
+                try
+                {
+                    NumberValidator<int>.IsPositive(value);
+                    _statusCode = value;
+                }
+                catch(ArgumentException exception)
+                {
+                    _message += $"\nERROR: {exception}";
+
+                }
+            }
+        }
+
+
+        private string _message;
+
+        public string Message
+        {
+            get
+            {
+                return _message;
+            }
+            set
+            {
+                try
+                {
+                    StringValidator.CheckIsNotNull(value);
+                    _message = value;
+                }
+                catch(ArgumentNullException exception)
+                {
+                    _message += $"\nERROR: {exception}";
+                }
+            }
+        }
+
+
+        public bool IsSuccess { get; set; }
+        
+        public override string ToString()
+        {
+            return JsonConvert.SerializeObject(this, Formatting.Indented);
+        }
+    }
+}
