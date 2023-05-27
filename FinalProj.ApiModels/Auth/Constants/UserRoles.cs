83ac6459baccd8f5d650a1b1ae1d4d8d116779af
ayubarr <114123666+ayubarr@users.noreply.github.com>
2023-05-27 22:43:40 +0600

    ADD: API MODELS project added ADD: Auth models added

---
+++ FinalProj.ApiModels/Auth/Constants/UserRoles.cs
@@ -0,0 +1,9 @@
+﻿namespace FinalApp.Api.Authentication
+{
+    public static class UserRoles
+    {
+        public const string Admin = "Admin";
+        public const string Moderator = "Moderator";
+        public const string User = "User";
+    }
+}
