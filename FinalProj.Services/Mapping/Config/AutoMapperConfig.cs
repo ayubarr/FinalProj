80f68c0ae62b3f532f6c50e1230bdf96b315fd03
ayubarr <114123666+ayubarr@users.noreply.github.com>
2023-05-27 23:39:40 +0600

    ADD: Map config class added

---
+++ FinalProj.Services/Mapping/Config/AutoMapperConfig.cs
@@ -0,0 +1,28 @@
+﻿using AutoMapper;
+using FinalApp.ApiModels.DTOs.CommonDTOs.BaseDTOs;
+using FinalApp.Domain.Models.Abstractions.BaseEntities;
+
+namespace FinalApp.Services.Mapping.Config
+{
+    /// <summary>
+    /// Configuration class for AutoMapper to map entities between two models.
+    /// </summary>
+    /// <typeparam name="Tmodel">Source model type.</typeparam>
+    /// <typeparam name="T">Destination model type.</typeparam>
+    public class AutoMapperConfig<T, Tmodel>
+    {
+        /// <summary>
+        /// Initializes and configures the AutoMapper with the specified mappings between the source entity type and destination DTO type.
+        /// </summary>
+        /// <returns>An instance of IMapper.</returns>
+        public static IMapper Initialize()
+        {
+            var mapperConfiguration = new MapperConfiguration(cfg =>
+            {
+                cfg.CreateMap<T, Tmodel>();
+            });
+
+            return mapperConfiguration.CreateMapper();
+        }
+    }
+}
