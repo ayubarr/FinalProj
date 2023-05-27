4da6d9872e8f90fcc68920faf3c0104ef30b2fd1
ayubarr <114123666+ayubarr@users.noreply.github.com>
2023-05-27 22:36:35 +0600

    ADD: StringValidator ADDED

---
+++ FinalProj.ValidationHelper/StringValidator.cs
@@ -0,0 +1,23 @@
+﻿namespace FinallApp.ValidationHelper
+{
+
+    /// <summary>
+    /// String validation.
+    /// </summary>
+    public static class StringValidator
+    {
+        /// <summary>
+        /// Checks if the string is not null or empty.
+        /// </summary>
+        /// <param name="text">The string to be checked.</param>
+        /// <exception cref="ArgumentNullException"></exception>
+        public static void CheckIsNotNull(string text)
+        {
+            if (string.IsNullOrEmpty(text))
+            {
+                throw new ArgumentNullException(nameof(text), "Параметр не должен быть равен Null");
+            }
+        }
+    }
+    
+}
