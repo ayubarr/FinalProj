0204ba8c214cec61ff3ea39e0166321fc9761c5f
ayubarr <114123666+ayubarr@users.noreply.github.com>
2023-05-27 23:02:40 +0600

    Add: Response Helper and Factory added

---
+++ FinalProj.ApiModels/DTOs/Response/Helpers/ResponseFactory.cs
@@ -0,0 +1,50 @@
+﻿using FinalApp.ApiModels.DTOs.CommonDTOs.BaseDTOs;
+using FinalApp.ApiModels.Response.Implemintations;
+
+namespace FinalApp.ApiModels.Response.Helpers
+{
+    public static class ResponseFactory<T>
+    {
+        public static BaseResponse<T> CreateSuccessResponse(T model)
+        {
+            return new BaseResponse<T>
+            {
+                IsSuccess = true,
+                Data = model,
+                StatusCode = 200,
+            };
+        }
+
+        public static BaseResponse<T> CreateNotFoundResponse(Exception exception)
+        {
+            return new BaseResponse<T>()
+            {
+                StatusCode = 404,
+                IsSuccess = false,
+                Message = "no records found in the database.\n\r" +
+                    $"Error: {exception}",
+            };
+        }
+
+        public static BaseResponse<T> CreateErrorResponse(Exception exception)
+        {
+            return new BaseResponse<T>()
+            {
+                StatusCode = 500,
+                IsSuccess = false,
+                Message = "internal server error.\n\r" +
+                $"Error: {exception}",
+            };
+        }
+        public static BaseResponse<T> CreateInvalidOperationResponse(Exception exception)
+        {
+            return new BaseResponse<T>()
+            {
+                StatusCode = 400,
+                IsSuccess = false,
+                Message = "Unable to create the specified model.\n\r" +
+                $"Error: {exception}",
+            };
+        }
+    }
+}
