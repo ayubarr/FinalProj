ffdd2a94620a4e7ea1cb9eba39c882e9c641b0e7
ayubarr <114123666+ayubarr@users.noreply.github.com>
2023-05-30 21:35:02 +0600

    add: Log added

---
+++ FinalProj/Models/Entities/Logs/Log.cs
@@ -0,0 +1,16 @@
+﻿using FinalApp.Domain.Models.Abstractions.BaseEntities;
+
+namespace FinalProj.Domain.Models.Entities.Logs
+{
+    public class Log : BaseEntity
+    {
+        public string LogLevel { get; set; }
+        public int? ThreadId { get; set; }
+        public int? EventId { get; set; }
+        public string? EventName { get; set; }
+        public string? Message { get; set; }
+        public string? ExceptionMessage { get; set; }
+        public string? ExceptionStackTrace { get; set; }
+        public string? ExceptionSource { get; set; }
+    }
+}
